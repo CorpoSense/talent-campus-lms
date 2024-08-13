1100e9ad9c1b3a7c2f848e82ae51dfdf0bbc62aa
lahcenbcf <h.bencharef@esi-sba.dz>
2024-08-13 14:10:10 +0100

    save

--- backend/api/authApi/views.py
+++ backend/api/authApi/views.py
@@ -1,18 +1,29 @@
-from django.shortcuts import render
 from .models import User
+from rest_framework.views import APIView
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated,AllowAny
-from rest_framework import generics
-
+from rest_framework import generics,permissions
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authentication import TokenAuthentication
+from django.contrib.auth import authenticate
 # Create your views here.
 
-#create a view to add a user
-# generic will do under the hood the creation of an user for us.
-class createUserView(generics.CreateAPIView):
-    # to make sure that will not create a user that already exist
-    querySet = User.objects.all()
-    # this serializer will tell this view which kind of data we accept
-    serialize_class = User
-    # who can call this function (view)
-    permissions = [AllowAny]
+class RegisterUserView(APIView):
+    serializer_class = UserSerializer
+    permission_classes=[permissions.AllowAny]
 
+
+class LoginUserView(APIView):
+    authentication_classes=[TokenAuthentication]
+    def post(self,request):
+        user = authenticate(email = request.data["email"],password=request.data["password"])
+        if(user):
+            token,created = Token.objects.get_or_create(user=user)
+            return Response({
+                "userId" : user.id,
+                "token":token
+            })
+        else :
+            return Response({
+                "error":True,"message":"Invalid Credentials"
+            })
--- backend/api/lms/admin.py
+++ backend/api/lms/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CourseCategorie,Course,Video,Enrollement,Recommendation,Review,Quiz,QuizChoice,QA,Subscription
+
+
+admin.site.register([CourseCategorie,Course,Video,Enrollement,Recommendation,Review,Quiz,QuizChoice,QA,Subscription])
 
 # Register your models here.
